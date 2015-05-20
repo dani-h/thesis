5b5d5a49d2f9d8631d873058a0a0fe5e2c4cefdc
Dani Hodovic <danihgit@gmail.com>
2015-05-20 13:21:27 +0200

    Added argparsing filetype filter
    Considering that we hardcoded arrays for different filetypes its more
    flexible to pass it as an arg to the script.
    
    Also added the whole tailed pearson value instead of only the
    correlation value from scipy.stats.pearsonr.

--- src/statistics.py
+++ src/statistics.py
@@ -37,7 +37,7 @@ def plot_new_window(x, y, suptitle='', title=''):
     fig.suptitle(suptitle)
 
     subplot = fig.add_subplot(111)
-    subplot.scatter(x.data, y.data, s=100)
+    subplot.scatter(x.data, y.data, s=30)
     subplot.set_xlim(xmin=0)
     subplot.set_ylim(ymin=0)
 
@@ -52,8 +52,9 @@ def create_data_entries(dataset, keys):
         d[key] = DataEntry(key)
     # Additional data we aggregate
     d['defects_abc'] = DataEntry('defects_abc')
-    d['defect_density'] = DataEntry('defect_density')
-    d['defect_density_abc'] = DataEntry('defect_density_abc')
+    d['defect_density_nloc'] = DataEntry('defect_density_nloc')
+    d['defect_density_nloc_abc'] = DataEntry('defect_density_nloc_abc')
+    d['defect_density_complexity_abc'] = DataEntry('defect_density_complexity_abc')
 
     for entry in dataset:
         for key in keys:
@@ -62,9 +63,12 @@ def create_data_entries(dataset, keys):
         defects_abc = entry['defects_a'] + entry['defects_b'] + entry['defects_c']
         defect_density = 0 if entry['nloc'] == 0 else entry['defects'] * 1.0 / entry['nloc']
         defect_density_abc = 0 if entry['nloc'] == 0 else defects_abc * 1.0 / entry['nloc']
+        defect_density_complexity_abc = 0 if entry['cyclomatic_complexity'] == 0 else defects_abc * 1.0 / entry['cyclomatic_complexity']
+
         d['defects_abc'].data.append(defects_abc)
-        d['defect_density'].data.append(defect_density)
-        d['defect_density_abc'].data.append(defect_density_abc)
+        d['defect_density_nloc'].data.append(defect_density)
+        d['defect_density_nloc_abc'].data.append(defect_density_abc)
+        d['defect_density_complexity_abc'].data.append(defect_density_complexity_abc)
 
     return d
 
@@ -97,9 +101,9 @@ def correlate_all(data_entries):
             as_set = {entry_x.label, entry_y.label}
             if as_set not in finished and entry_x != entry_y:
                 pearson_dict = {}
-                pearson_dict['value'] = pearsonr(entry_x.data, entry_y.data)[0]
-                pearson_dict['label'] = "x: {0:30s} y: {1:30s} Pearson correlation: {2:.2f}".format(
-                        entry_x.label, entry_y.label, pearson_dict['value'])
+                pearson_dict['value'] = pearsonr(entry_x.data, entry_y.data)
+                pearson_dict['label'] = "x: {0:30s} y: {1:30s} Pearson correlation: {2:.2f} P value: {3}".format(
+                        entry_x.label, entry_y.label, pearson_dict['value'][0], pearson_dict['value'][1])
                 correlations.append(pearson_dict)
 
             finished.append(as_set)
@@ -121,6 +125,8 @@ if __name__ == '__main__':
     argsparser = argparse.ArgumentParser(description=DESCRIPTION)
     argsparser.add_argument('--file', type=str,
             help='File where the data is located, if omitted request to the local Django server will be made')
+    argsparser.add_argument('--filter', nargs="+", type=str,
+            help='Filter the files to these file endings. Usage: --filter .a .b .c. Recommended: .c, .cc, cpp, .cxx, .sbs.')
     argsparser.add_argument('-c', action='store_true',
             help='Prints the pearson correlation values for all metrics')
     argsparser.add_argument('-p', action='store_true',
@@ -129,32 +135,28 @@ if __name__ == '__main__':
     args = argsparser.parse_args()
     json = get_data(args.file)
 
-    jsonarr_srcfiles = filter(lambda entry: entry['file'].endswith((".cc", ".cpp", ".c", ".sbs")), json)
-    jsonarr_handwritten = filter(lambda entry: entry['file'].endswith((".cc", ".cpp", ".c")), json)
-    jsonarr_sbs = filter(lambda entry: entry['file'].endswith(".sbs"), json)
+    if args.filter:
+        print "-" * 40
+        print "Only using these filetypes", args.filter
+        print "-" * 40
+        jsonarr = filter(lambda entry: entry['file'].endswith(tuple(args.filter)), json)
+    else:
+        jsonarr = json
 
     db_metrics = ['contributors_tr', 'contributors_cm', 'defects', 'defects_a', 'defects_b', 'defects_c',
-            'defects_improvement', 'nloc', 'cyclomatic_complexity']
+            'defects_improvement', 'nloc', 'cyclomatic_complexity', 'effective_complexity']
 
-    dict_arr_all = create_data_entries(json, db_metrics)
-    dict_arr_srcfiles = create_data_entries(jsonarr_srcfiles, db_metrics)
-    dict_arr_handwritten = create_data_entries(jsonarr_handwritten, db_metrics)
-    dict_arr_sbs = create_data_entries(jsonarr_sbs, db_metrics)
+    # Dict of arrays containing values, incl aggregated values not found in the source data
+    dict_arr = create_data_entries(jsonarr, db_metrics)
 
     if args.c:
-        correlations = correlate_all(dict_arr_handwritten)
+        correlations = correlate_all(dict_arr)
         sorted_correlations = sorted(correlations, key=lambda entry: entry['value'])
         for correlation in sorted_correlations:
             print correlation['label']
 
     if args.p:
-        plot_new_window(dict_arr_all['contributors_tr'], dict_arr_all['defect_density_abc'],
+        plot_new_window(dict_arr['contributors_tr'], dict_arr['defects_abc'],
                 suptitle="Contributors TR and defects a,b,c for all files")
-        plot_new_window(dict_arr_handwritten['contributors_tr'], dict_arr_handwritten['defect_density_abc'],
-                suptitle="Contributors TR and defect density a,b,c for source files (c, cpp, cc)")
-        plot_new_window(dict_arr_handwritten['contributors_tr'], dict_arr_handwritten['defects_abc'],
-                suptitle="Contributors TR and defects a,b,c for source files")
-        plot_new_window(dict_arr_handwritten['contributors_tr'], dict_arr_handwritten['cyclomatic_complexity'],
-                suptitle="Contributors TR and cyclomatic complexity")
         plot.show()
 
