300e550204bbbb257425ea9a4bab39efcd70a7ae
Dani Hodovic <danihgit@gmail.com>
2015-05-23 22:50:09 +0200

    Added pearson metadata plot
    Code needs to be cleaned up.

--- src/statistics.py
+++ src/statistics.py
@@ -2,6 +2,7 @@
 
 import requests
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plot
 import sys
 import simplejson as json
@@ -16,7 +17,7 @@ class DataEntry():
         self.data = []
 
 
-def normalize_json(json_arr):
+def normalize_json(json_arr, aggregate_func=None):
     keys = json_arr[0]
     values = json_arr[1:len(json_arr)]
     json = []
@@ -24,66 +25,69 @@ def normalize_json(json_arr):
         entry = {}
         for idx in range(0, len(keys)):
             entry[keys[idx]] = val[idx]
+
+        if aggregate_func: aggregate_func(entry)
         json.append(entry)
     return json
 
 
-def plot_new_window(x, y, suptitle='', title=''):
-    '''
-    :type x: DataEntry
-    :type y: DataEntry
-    '''
+def aggregate_values(entry):
+    entry['defect_density'] = 0
+    entry['defect_density_complexity'] = 0
+    defects_abc = entry['defects_a'] + entry['defects_b'] + entry['defects_c']
+    entry['defects_abc'] = defects_abc
+
+    if entry['nloc'] > 0:
+        entry['defect_density'] = (defects_abc * 1.0) / entry['nloc']
+
+    if entry['cyclomatic_complexity'] > 0:
+        entry['defect_density_complexity'] = (defects_abc * 1.0) / entry['cyclomatic_complexity']
+
+
+def plot_new_window(x, y, xlabel='', ylabel='', suptitle='', title=''):
     fig = plot.figure()
     fig.suptitle(suptitle)
 
     subplot = fig.add_subplot(111)
-    subplot.scatter(x.data, y.data, s=30)
+    subplot.scatter(x, y, s=30)
     subplot.set_xlim(xmin=0)
     subplot.set_ylim(ymin=0)
 
     subplot.set_title(title)
-    subplot.set_xlabel(x.label)
-    subplot.set_ylabel(y.label)
+    subplot.set_xlabel(xlabel)
+    subplot.set_ylabel(ylabel)
 
 
-def create_data_entries(dataset, keys):
+def create_data_entries(dataset, excluded_keys=None):
     d = {}
+    if excluded_keys is None:
+        keys = dataset[0].keys()
+    else:
+        keys = set(dataset[0].keys()) - set(excluded_keys)
+
     for key in keys:
         d[key] = DataEntry(key)
-    # Additional data we aggregate
-    d['defects_abc'] = DataEntry('defects_abc')
-    d['defect_density_nloc'] = DataEntry('defect_density_nloc')
-    d['defect_density_nloc_abc'] = DataEntry('defect_density_nloc_abc')
-    d['defect_density_complexity_abc'] = DataEntry('defect_density_complexity_abc')
 
     for entry in dataset:
         for key in keys:
             d[key].data.append(entry[key])
 
-        defects_abc = entry['defects_a'] + entry['defects_b'] + entry['defects_c']
-        defect_density = 0 if entry['nloc'] == 0 else entry['defects'] * 1.0 / entry['nloc']
-        defect_density_abc = 0 if entry['nloc'] == 0 else defects_abc * 1.0 / entry['nloc']
-        defect_density_complexity_abc = 0 if entry['cyclomatic_complexity'] == 0 else defects_abc * 1.0 / entry['cyclomatic_complexity']
-
-        d['defects_abc'].data.append(defects_abc)
-        d['defect_density_nloc'].data.append(defect_density)
-        d['defect_density_nloc_abc'].data.append(defect_density_abc)
-        d['defect_density_complexity_abc'].data.append(defect_density_complexity_abc)
-
     return d
+
 
 def get_data(filename=None):
     if filename:
         with open(filename, 'r') as f:
-            return normalize_json(json.load(f))
+            return json.load(f)
     else:
         try:
             url = "http://localhost:8000/contributors_defects_data"
             req = requests.get(url)
-            return normalize_json(req.json())
+            return req.json()
         except:
             print 'Failed to make a request to the local at %s' % (url)
             sys.exit(1)
+
 
 def correlate_all(data_entries):
     '''
@@ -109,6 +113,45 @@ def correlate_all(data_entries):
             finished.append(as_set)
     return correlations
 
+
+def pearson_metadata(jsonarr):
+    '''
+    @Todo: Refactor, written in a hurry
+
+    Correlate:
+        x axis nloc
+        y axis pearsonnr defects/contributors
+
+    :type jsonarr: []
+    '''
+    INCR = 10
+    sizes = 0
+    max_nloc = max(map(lambda entry: entry['nloc'], jsonarr))
+    xvals = []
+    yvals = []
+    while sizes < max_nloc:
+        filtered = filter(lambda entry: entry['nloc'] > sizes, jsonarr)
+        contributors_arr = map(lambda entry: entry['contributors_tr'], filtered)
+        defect_density_arr = map(lambda entry: entry['defect_density'], filtered)
+        pearson_val = pearsonr(contributors_arr, defect_density_arr)
+        xvals.append(sizes)
+        yvals.append(pearson_val[0])
+        sizes += INCR
+
+    fig = plot.figure(facecolor='white')
+    subplot = fig.add_subplot(111)
+    subplot.plot(xvals, yvals)
+    subplot.set_title('Pearson metadata')
+    subplot.set_xlabel('LOC')
+    subplot.set_ylabel('Peason correlation coefficient for defect density/contributors')
+    subplot.set_xlim(xmin=0, xmax=15000)
+    subplot.set_ylim(ymin=0, ymax=1)
+    font = {'size': 22}
+    matplotlib.rc('font', **font)
+    plot.show()
+
+
+
 if __name__ == '__main__':
     DESCRIPTION = '''
     Script that correlates and plots various contributor and defects data.
@@ -126,14 +169,17 @@ if __name__ == '__main__':
     argsparser.add_argument('--file', type=str,
             help='File where the data is located, if omitted request to the local Django server will be made')
     argsparser.add_argument('--filter', nargs="+", type=str,
-            help='Filter the files to these file endings. Usage: --filter .a .b .c. Recommended: .c, .cc, cpp, .cxx, .sbs.')
+            help='Filter the files to these file extensions. Usage: .c, .cc, cpp, .cxx, .sbs.')
     argsparser.add_argument('-c', action='store_true',
             help='Prints the pearson correlation values for all metrics')
     argsparser.add_argument('-p', action='store_true',
             help='Plots scatter charts for some of the metrics')
+    argsparser.add_argument('-metadata', action='store_true',
+            help='Plots a chart with pearson metadata')
 
     args = argsparser.parse_args()
-    json = get_data(args.file)
+    jsondata_nonformatted = get_data(args.file)
+    json = normalize_json(jsondata_nonformatted, aggregate_values)
 
     if args.filter:
         print "-" * 40
@@ -143,11 +189,8 @@ if __name__ == '__main__':
     else:
         jsonarr = json
 
-    db_metrics = ['contributors_tr', 'contributors_cm', 'defects', 'defects_a', 'defects_b', 'defects_c',
-            'defects_improvement', 'nloc', 'cyclomatic_complexity', 'effective_complexity']
-
-    # Dict of arrays containing values, incl aggregated values not found in the source data
-    dict_arr = create_data_entries(jsonarr, db_metrics)
+    excluded_keys = ['id', 'file', 'subsystem_id', 'subsystem']
+    dict_arr = create_data_entries(jsonarr, excluded_keys)
 
     if args.c:
         correlations = correlate_all(dict_arr)
@@ -156,7 +199,10 @@ if __name__ == '__main__':
             print correlation['label']
 
     if args.p:
-        plot_new_window(dict_arr['contributors_tr'], dict_arr['defects_abc'],
-                suptitle="Contributors TR and defects a,b,c for all files")
+        plot_new_window(dict_arr['contributors_cm'].data, dict_arr['defects_abc'].data,
+                suptitle="Contributors cm and defects a,b,c")
         plot.show()
+
+    if args.metadata:
+        pearson_metadata(jsonarr)
 
